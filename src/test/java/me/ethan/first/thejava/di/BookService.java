c23259c2dddc2d11833449f94b76b449eeab172d
EthanKim <hey0507@gmail.com>
2022-05-10 23:43:04 +0900

    fix : 5/10 테스트 코드 변경 - 기존 @Test 어노테이션을 통해 임포트 되는 라이브러리가 달라서, 테스트 코드로 인증이 안되는 부분 확인 및 수정
    org.Junit.Test 에서 org.junit.jupiter.api.Test 로 변경

--- src/test/java/me/ethan/first/thejava/di/BookService.java
+++ src/test/java/me/ethan/first/thejava/di/BookService.java
@@ -1,5 +1,7 @@
 package me.ethan.first.thejava.di;
 
+import me.ethan.first.thejava.di.Inject;
+
 public class BookService {
 
     @Inject
--- src/test/java/me/ethan/first/thejava/di/ContainerServiceTest.java
+++ src/test/java/me/ethan/first/thejava/di/ContainerServiceTest.java
@@ -1,24 +1,19 @@
 package me.ethan.first.thejava.di;
-
-import org.junit.Test;
-
+import org.junit.jupiter.api.Test; // Org.Junit 라이브러리와 차이가 있음을 기억
 import static org.junit.Assert.*;
-
-
 class ContainerServiceTest {
 
     @Test
-    public  void getObject_BookRepository(){
+    public void getObject_BookRepository(){
         BookRepository bookRepository = ContainerService.getObject(BookRepository.class);
         assertNotNull(bookRepository);
     }
 
     @Test
-    public  void getObject_BookService(){
+    public void getObject_BookService(){
         BookService bookService = ContainerService.getObject(BookService.class);
         assertNotNull(bookService);
         assertNotNull(bookService.bookRepository);
-
     }
 
 }
